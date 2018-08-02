53ad4f8ab8f83cbdcff38b5aef9f93df067a7d9f
Kou Muu <koumuu.10@gmail.com>
2018-08-02 16:29:33 +0000

    output result like

--- evaluator.ml
+++ evaluator.ml
@@ -34,7 +34,50 @@ let mgu (a, b) =
     | (_, _) -> (false, unifier)
   in ut ([a], [b], (fun x -> x))
 
-let succeed query = (print_string ((ast2string query) ^ ".\n"); true)
+let rec get_vars ast_list = match ast_list with
+  (App(_, (_ as arg1)::(_ as arg2)::_))::rest -> (get_vars [arg1])@(get_vars [arg2])@(get_vars rest)
+  | (Var var)::rest -> (Var var)::(get_vars rest)
+  | _ -> []
+
+let rec map_vars (vars, unifier) =
+  begin
+    List.fold_left 
+      (fun vars_mapping var -> 
+        ((var, unifier var)::vars_mapping))
+      []
+      vars
+  end 
+
+let succeed vars_mapping = 
+  begin
+    let rec filter_var var mapping = match mapping with
+      (Var key, Var value)::rest ->
+        if var = key then
+          (filter_var value rest)
+        else
+          (filter_var var rest)
+      | (Var key, Atom value)::rest ->
+        if var = key then value
+        else ((filter_var var rest))
+      | (_, _)::rest -> (assert false)
+      | [] -> (assert false)
+    in let rec filter_vars mapping = match mapping with
+      (Var key, _)::rest ->
+        if (String.contains key '#') then []
+        else (key, filter_var key mapping)::(filter_vars rest)
+      | (_, _)::rest -> (assert false)
+      | [] -> []
+    in 
+    print_string (List.fold_left 
+      (fun sentence (name, value) -> 
+        P.sprintf "%s%s = %s\n" 
+          sentence 
+          name 
+          value)
+      ""
+      (filter_vars vars_mapping));
+    true
+  end
 
 let rename ver term =
   let rec mapVar ast = match ast with
@@ -43,19 +86,21 @@ let rename ver term =
     | (App(n, terms)) -> App(n, List.map mapVar terms)
   in mapVar term
 
-let rec solve (program, question, result, depth) = match question with
-  [] -> succeed result
+let rec solve (program, question, depth, vars_mapping) = match question with
+  [] -> succeed vars_mapping
   | goal::goals ->
     let onestep _ clause = match List.map (rename (string_of_int depth)) clause with
       [] -> raise Compiler_error
       | head::conds ->
         let (unifiable, unifier) = mgu(head, goal) in
           if unifiable then
-            solve (program, List.map unifier (conds@goals), unifier result, depth+1)
+            let unified_question = (List.map unifier (conds@goals)) in
+            solve (program, unified_question, depth+1, vars_mapping@(map_vars ((get_vars question), unifier)))
           else
             true
-        in List.fold_left onestep true program
+    in List.fold_left onestep true program
 
-let eval (program, question) = solve(program, [question], question, 1)
+let eval (program, question) = solve(program, [question], 1, [])
+
 
 end
